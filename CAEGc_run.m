3a9f7490247e129f4738afe3b5e945b1b3cad932
Jiahao Li <302914353@qq.com>
2023-04-11 21:27:07 +0800

    Update CAEGc_run.m

--- CAEGc_run.m
+++ CAEGc_run.m
@@ -42,7 +42,7 @@ eta_max = 0.2;
 
 s = cell(1,N);
 for ns = 1:N
-    s{1,ns} = 0; 
+    s{1,ns} = 0;
 end
 
 num_eta=0;
@@ -57,7 +57,7 @@ end
 
 S_o = cell(1,N);
 for nS = 1:N
-    S_o{1,nS} = ones(num_eta,1);   
+    S_o{1,nS} = ones(num_eta,1);
 end
 
 e = cell(T,num_eta);
@@ -77,18 +77,18 @@ for t = 1:T
 %         exp_cumres(:,:,t) = daliy_exp_r(:,:,t);
 
     else 
-        k=0;                                                             
+        k=0;
         for eta = eta_min:step:eta_max
             k = k+1;
             ff = e{t-1,k};
             Z = ff.*exp(eta*data(t-1,:)/(ff*data(t-1,:)'))*ones(N,1);
             f = ff.*exp(eta*data(t-1,:)/(ff*data(t-1,:)'));
-            f = f/Z; 
+            f = f/Z;
             e{t,k} = f;
 
             exp_h(t-1,:)=data(t-1,:).*e{t-1,k};
             exp_hat(t-1,:)=exp_h(t-1,:)/sum(exp_h(t-1,:));
-            exp_diff(t,:)=sum(abs(e{t,k}-exp_hat(t-1,:)));  
+            exp_diff(t,:)=sum(abs(e{t,k}-exp_hat(t-1,:)));
 
             for n1 = 1:N
                 if t <= 2
@@ -97,7 +97,7 @@ for t = 1:T
                     c_min=(tc*exp_diff(t-1,:))/(1+tc);
                     c_max=(tc*exp_diff(t-1,:))/(1-tc);
                     [c,~]=fminbnd(@(c) abs(S{1,n1}(k,1)*c-tc*S{1,n1}(k,1)*...
-                        sum(abs(ff*(1-c)-exp_hat(t-2,:)))),c_min,c_max); 
+                        sum(abs(ff*(1-c)-exp_hat(t-2,:)))),c_min,c_max);
                     S{1,n1}(k,1) = S{1,n1}(k,1)*(ff*data(t-1,:)'*(1-c));
                 end
             end
@@ -120,13 +120,13 @@ for t = 1:T
 
         b_h(t-1,:) = data(t-1,:).*b(t-1,:);
         b_hat(t-1,:) = b_h(t-1,:)/sum(b_h(t-1,:));
-        diff(t,:) = sum(abs(b(t,:)-b_hat(t-1,:)));    
+        diff(t,:) = sum(abs(b(t,:)-b_hat(t-1,:)));
         
         c_min(t,:) = (tc*diff(t,:))/(1+tc);
         c_max(t,:) = (tc*diff(t,:))/(1-tc);
         [c,~] = fminbnd(@(c) abs(total_return(t-1,:)*c-tc*...
             total_return(t-1,:)*sum(abs(b(t,:)*(1-c)-b_hat(t-1,:)))),...
-            c_min(t,:),c_max(t,:));        
+            c_min(t,:),c_max(t,:));
         c1(t,1) = c;
         
         daily_return(t) = data(t,:)*b(t,:)'*(1-c);
